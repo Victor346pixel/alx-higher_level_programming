350a04c6fffd6460304af2838398c935ce98e042
victor346pixel <monicamwendia84@gmail.com>
2023-06-05 15:41:16 -0700

    hurray just updated python

---
+++ 0x00-python-hello_world/lists.h
@@ -0,0 +1,24 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stdlib.h>
+
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+*/
+
+typedef struct listint_s
+{
+	int n;
+	struct listint_s *next;
+} listint_t;
+
+void free_listint(listint_t *head);
+size_t print_listint(const listint_t *h);
+int check_cycle(listint_t *list);
+listint_t *add_nodeint(listint_t **head, const int n);
+
+#endif /*LISTS_H*/
