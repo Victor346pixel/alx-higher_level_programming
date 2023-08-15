fd356b476a40504a1d7f1d3e735dfbead5cc35e4
Victor346pixel <monicamwaniki84@gmail.com>
2023-08-15 23:10:16 +0300

    updated

--- 0x0D-SQL_introduction/8-count_89.sql
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -1,2 +1,2 @@
 -- displays  number of records id = 89 in table first_table of  database hbtn_0c_0 in MySQL server.
-COUNT * FROM first_table WHERE id = 89;
+SELECT COUNT * FROM first_table WHERE id = 89;
--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,5 +1,4 @@
 --  script that creates a table second_table in the database hbtn_0c_0 in your MySQL server and add multiples rows.
-
 CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
 
 -- should create four records
