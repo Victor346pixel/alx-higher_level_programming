92ce4d5370edd3601c2cecdf4c82086b28652053
Victor346pixel <monicamwaniki84@gmail.com>
2023-09-29 22:25:55 +0300

    updated

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
-# Get the byte size of the HTTP response header for a given URL.
-curl -s "$1" | wc -c
+
+# Get the URL from the command line argument
+url=$1
+
+# Send a request to the URL and capture the response body
+response=$(curl -s $url)
+
+# Get the size of the response body in bytes
+body_size=${#response}
+
+# Display the size of the response body
+echo $body_size
--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
-# Get the response body for a given URL for 200 status code responses.
-curl -sL "$1"
+
+# Check if the URL argument is provided
+if [ $# -ne 1 ]; then
+    echo "Usage: $0 <URL>"
+    exit 1
+fi
+
+# Send a GET request to the provided URL and store the response in a variable
+response=$(curl -s -o /dev/null -w "%{http_code}" "$1")
+
+# Check if the HTTP response code is 200 (OK)
+if [ "$response" -eq 200 ]; then
+    # If the response code is 200, display the body of the response
+    curl -s "$1"
+else
+    echo "HTTP response code: $response"
+fi
