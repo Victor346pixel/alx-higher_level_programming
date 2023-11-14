458408cb6de8a1874e50334394cd96046ccea0b6
Victor346pixel <monicamwaniki84@gmail.com>
2023-11-14 13:37:31 +0300

    fixed errors

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-if (Process.argv.length === 2) {
-    console.log("No arguments");
+if (process.argv.length === 2) {
+    console.log('No argument');
 } else if (process.argv.length === 3) {
-    console.log("Argument found");
+    console.log('Argument found');
 } else {
-    console.log("Arguments found");
+    console.log('Arguments found');
 }
