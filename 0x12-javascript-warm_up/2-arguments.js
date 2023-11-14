ff1b3f79c92f6dd2e736af830d38bffe2d9a2578
Victor346pixel <monicamwaniki84@gmail.com>
2023-11-14 13:52:06 +0300

    updated

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -5,8 +5,4 @@ if (process.argv.length === 2) {
     console.log('Argument found');
 } else {
     console.log('Arguments found');
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> 0cb823c78d5feb862ec3774740c37ae3b3eba92f
